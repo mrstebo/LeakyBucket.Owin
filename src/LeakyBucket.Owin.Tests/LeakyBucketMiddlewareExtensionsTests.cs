eb89c3e7310a4bd24c5214bc8cdb844842de1cf1
Steve Atkinson <steve.atkinson@dootrix.com>
2018-01-14 14:23:36 +0000

    Added extension for easily adding the middleware to the Owin app. Added tests for the new extension method.

---
+++ src/LeakyBucket.Owin.Tests/LeakyBucketMiddlewareExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using FakeItEasy;
+using LeakyBucket.Owin.Identifiers;
+using LeakyBucket.Owin.Store;
+using Microsoft.Owin.Testing;
+using NUnit.Framework;
+using Owin;
+
+namespace LeakyBucket.Owin.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class LeakyBucketMiddlewareExtensionsTests
+    {
+        [Test]
+        public async Task UseLeakyBucket_should_attach_middleware()
+        {
+            var store = A.Fake<IRequestStore>();
+            var configuration = new LeakyBucketConfiguration
+            {
+                RequestStore = store
+            };
+
+            using (var server = TestServer.Create(app =>
+            {
+                app.UseLeakyBucket(configuration);
+                app.Run(async ctx => await ctx.Response.WriteAsync("Test"));
+            }))
+            {
+                await server.CreateRequest("/").GetAsync();
+            }
+
+            A.CallTo(() => store.NumberOfRequestsFor(A<IClientIdentifier>.Ignored))
+                .MustHaveHappened(Repeated.Exactly.Once);
+        }
+    }
+}
---
+++ src/LeakyBucket.Owin/LeakyBucketMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace LeakyBucket.Owin
+{
+    public static class LeakyBucketMiddlewareExtensions
+    {
+        public static IAppBuilder UseLeakyBucket(this IAppBuilder app, LeakyBucketConfiguration configuration)
+        {
+            return app.Use(typeof(LeakyBucketMiddleware), configuration);
+        }
+    }
+}
