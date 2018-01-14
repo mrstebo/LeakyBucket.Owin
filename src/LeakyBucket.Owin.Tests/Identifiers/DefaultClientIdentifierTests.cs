801190937ff69279d76d42b34c42c66b368e3cbe
Steve Atkinson <steve.atkinson@dootrix.com>
2018-01-14 14:50:41 +0000

    Added tests for the DefaultClientIdentifier and DefaultRequestStore.

---
+++ src/LeakyBucket.Owin.Tests/Identifiers/DefaultClientIdentifierTests.cs
@@ -0,0 +1,28 @@
+﻿using LeakyBucket.Owin.Identifiers;
+using NUnit.Framework;
+
+namespace LeakyBucket.Owin.Tests.Identifiers
+{
+    [TestFixture]
+    [Parallelizable]
+    public class DefaultClientIdentifierTests
+    {
+        [Test]
+        public void Equals_should_return_true_if_other_has_same_UserAgent()
+        {
+            var identifier = new DefaultClientIdentifier("test");
+            var other = new DefaultClientIdentifier("test");
+
+            Assert.IsTrue(identifier.Equals(other));
+        }
+
+        [Test]
+        public void Equals_should_return_false_if_other_has_different_UserAgent()
+        {
+            var identifier = new DefaultClientIdentifier("test");
+            var other = new DefaultClientIdentifier("other");
+
+            Assert.IsFalse(identifier.Equals(other));
+        }
+    }
+}
---
+++ src/LeakyBucket.Owin.Tests/Store/DefaultRequestStoreTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using FakeItEasy;
+using LeakyBucket.Owin.Identifiers;
+using LeakyBucket.Owin.Store;
+using NUnit.Framework;
+
+namespace LeakyBucket.Owin.Tests.Store
+{
+    [TestFixture]
+    [Parallelizable]
+    public class DefaultRequestStoreTests
+    {
+        [Test]
+        public void NumberOfRequestsFor_should_return_number_of_requests_for_identifier()
+        {
+            var identifier = A.Fake<IClientIdentifier>();
+            var store = new DefaultRequestStore(5);
+
+            store.AddRequest(identifier, DateTime.UtcNow);
+            store.AddRequest(identifier, DateTime.UtcNow);
+
+            var result = store.NumberOfRequestsFor(identifier);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void AddRequest_should_add_requests_for_identifier()
+        {
+            var identifier = A.Fake<IClientIdentifier>();
+            var store = new DefaultRequestStore(1);
+
+            store.AddRequest(identifier, DateTime.UtcNow);
+
+            Assert.AreEqual(1, store.NumberOfRequestsFor(identifier));
+        }
+
+        [Test]
+        public void AddRequest_should_not_exceed_max_number_of_requests()
+        {
+            var identifier = A.Fake<IClientIdentifier>();
+            var store = new DefaultRequestStore(2);
+            
+            store.AddRequest(identifier, DateTime.UtcNow);
+            store.AddRequest(identifier, DateTime.UtcNow);
+            store.AddRequest(identifier, DateTime.UtcNow);
+
+            Assert.AreEqual(2, store.NumberOfRequestsFor(identifier));
+        }
+
+        [Test]
+        public void DeleteRequestsOlderThan_should_remove_requests_from_queue_older_than_date()
+        {
+            var identifier = A.Fake<IClientIdentifier>();
+            var store = new DefaultRequestStore(10);
+            var oldDateTime = new DateTime(2017, 1, 1, 12, 0, 0);
+            var currentDateTime = new DateTime(2017, 1, 1, 13, 0, 0);
+            
+            store.AddRequest(identifier, oldDateTime);
+            store.AddRequest(identifier, oldDateTime);
+            store.AddRequest(identifier, oldDateTime);
+            store.AddRequest(identifier, currentDateTime);
+            store.AddRequest(identifier, currentDateTime);
+
+            store.DeleteRequestsOlderThan(identifier, currentDateTime);
+
+            Assert.AreEqual(2, store.NumberOfRequestsFor(identifier));
+        }
+    }
+}
